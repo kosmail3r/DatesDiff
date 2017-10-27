1cb12dbbbc3dee176293f4b81b724d282a322555
kosmailer <kosmailer@gmail.com>
2017-10-28 01:37:05 +0300

    Getter/setter

--- Base.php
+++ Base.php
@@ -30,4 +30,98 @@ class Base
      */
     private $invert;
 
+    /**
+     * @var string
+     */
+    public $strDate;
+
+    /**
+     * Base constructor.
+     * @param string $strDate
+     */
+    public function __construct($strDate)
+    {
+        $this->strDate = $strDate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYears()
+    {
+        return $this->years;
+    }
+
+    /**
+     * @param int $years
+     */
+    public function setYears($years)
+    {
+        $this->years = $years;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMonths()
+    {
+        return $this->months;
+    }
+
+    /**
+     * @param int $months
+     */
+    public function setMonths($months)
+    {
+        $this->months = $months;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDays()
+    {
+        return $this->days;
+    }
+
+    /**
+     * @param int $days
+     */
+    public function setDays($days)
+    {
+        $this->days = $days;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalDays()
+    {
+        return $this->total_days;
+    }
+
+    /**
+     * @param int $total_days
+     */
+    public function setTotalDays($total_days)
+    {
+        $this->total_days = $total_days;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isInvert()
+    {
+        return $this->invert;
+    }
+
+    /**
+     * @param bool $invert
+     */
+    public function setInvert($invert)
+    {
+        $this->invert = $invert;
+    }
+
 }
