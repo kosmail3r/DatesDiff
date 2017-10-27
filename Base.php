a1a26d6ef4ec8ef726297bd4536425831973ea6f
kosmailer <kosmailer@gmail.com>
2017-10-28 01:28:52 +0300

    Initial commit

---
+++ Base.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: konstantin
+ * Date: 10/28/17
+ * Time: 1:28 AM
+ */
+
+class Base
+{
+
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: konstantin
+ * Date: 10/28/17
+ * Time: 1:27 AM
+ */
